7dc7b073a7131f173c6355ff3a6a8c6061edc676
Amir Rabiee <amir663rabiee@aol.com>
2024-02-02 18:30:07 +0330

    completed web crawler

--- services/puppeteer.js
+++ services/puppeteer.js
@@ -6,6 +6,7 @@ const browser = async () => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  let links= []
   await page.addScriptTag({
     url: "https://code.jquery.com/jquery-3.2.1.min.js",
   });
@@ -21,22 +22,39 @@ const browser = async () => {
     ) {
       var crawler =
         crawlersList[crawlersListPosition].crawlers[crawlerPosition];
+        
       for (let i = crawler.from; i <= crawler.to; i++) {
         await page.goto(
           crawler.staticAddress + crawler.dynamicAddress.replace("*", i),
           { waitUntil: "domcontentloaded" }
         );
-        let linkScript = crawler.linkScript
-        console.log(
-          await page.evaluate((linkScript) => {
-            var urls = [];
-            eval("$('.product-list-cont .item-cont .item-image .lnk-image').each((idx, el) => {   const selectedProduct = $(el);   let url = selectedProduct.attr('href').trim();    urls.push(url); }) ");
-            return urls;
-          })
-        );
+        const linkCrawler =crawler.linkScript
+        
+         links.push({crawlerId:crawler._id.toString(), links: await page.evaluate((linkCrawler) => {
+          var urls =[]
+          eval(linkCrawler)
+          return urls;
+        } , linkCrawler)} )
       }
+      
     }
   }
+  let crawleddata = []
+  for(let index = 0 ; index<links.length ; index++){
+    for(let link = 0 ; link< links[index].links.length ; link++){
+      await page.goto(links[index].links[link])
+      const dataCrawler =crawler.productPageScript
+      crawleddata.push({crawlerId:links[index].crawlerId, links: await page.evaluate((dataCrawler) => {
+        var productTitle = "" ; var image = [] ; var price = 0 ; var salesPrice = 0;var description ="" ;
+        eval(dataCrawler)
+        return {productTitle:productTitle , image:image , price:price , salesPrice:salesPrice , description:description};
+      } , dataCrawler)} )
+    }
+  }
+  console.log(crawleddata)
+
+
+
 
   // // Navigate the page to a URL
   // await page.goto("https://kalatik.com/category/26/samsung");
